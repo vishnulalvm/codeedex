4c65a2969e9c1966acf3ce09b68e8399a10baa64
Vishnulal  V M <vishnulalvm007@gmail.com>
2026-01-15 15:19:24 +0530

    First commit

---
+++ android/app/src/main/kotlin/codeedextechnologies/com/codeedex/MainActivity.kt
@@ -0,0 +1,5 @@
+package codeedextechnologies.com.codeedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CodeEdex',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CodeEdex'),
+      ),
+      body: const Center(
+        child: Text('Welcome to CodeEdex'),
+      ),
+    );
+  }
+}
